3310c1526060afc6b5aa7839d273bd9c70affb97
GroovyArea <095201a@naver.com>
2023-03-29 00:03:49 +0900

    feat : Project Init

---
+++ src/main/kotlin/com/account/manage/accountmanage/AccountManageApplication.kt
@@ -0,0 +1,11 @@
+package com.account.manage.accountmanage
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AccountManageApplication
+
+fun main(args: Array<String>) {
+    runApplication<AccountManageApplication>(*args)
+}
---
+++ src/test/kotlin/com/account/manage/accountmanage/AccountManageApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.account.manage.accountmanage
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AccountManageApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
